fc1c93b72a58c6aaccf961438771a5b1c13c9500
aliqnbri <aliqnbri1998@gmail.com>
2023-12-12 23:15:34 +0330

    Edited models menu,storage and  moneyfield added to menu.

--- cafe/models.py
+++ cafe/models.py
@@ -1,32 +1,34 @@
 from django.db import models
-
+from moneyfield import MoneyField
 # Create your models here.
+
+
 class Account(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=11)
 
+
 class Menu(models.Model):
     title = models.CharField(max_length=100)
-    price = models.IntegerField()
+    price = models.MoneyField(
+        decimal_places=2, max_digits=8, currency_default='Rial')
     description = models.TextField()
-    materials = models.models.IntegerField(primary_key=True)
+
 
 class Tables(models.Model):
     name = models.CharField(max_length=100)
     capacity = models.IntegerField()
     url = models.CharField()
-    
+
+
 class Reservation(models.Model):
-    account_id = models.ForeignKey(Account)
-    menu_id = models.ForeignKey(Menu)
-    table_id = models.ForeignKey(Tables , null=True)
+    account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+    menu_id = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    table_id = models.ForeignKey(Tables, null=True)
     created_at = models.DateTimeField(auto_created=True)
 
-class Stroage(models.Model):
-    menu_id = models.ManyToOneRel(Menu)
+
+class Storage(models.Model):
     material_title = models.CharField()
     remined_material = models.IntegerField()
     expire_date = models.DateField()
-        
-
-
