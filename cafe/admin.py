9156637ff05fde571a0ae731caf332f7f368cd2a
ImanSMD <110537853+ImanSMD@users.noreply.github.com>
2023-12-13 20:10:30 +0330

    all models in cafe app registered to admin.py

--- cafe/admin.py
+++ cafe/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Account)
+admin.site.register(Menu)
+admin.site.register(Tables)
+admin.site.register(Reservation)
+admin.site.register(Storage)
