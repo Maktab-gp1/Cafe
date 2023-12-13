262c9aa86b5c09ac2fa142a1ae38ff2dd3068b38
Amirznd <zenderoohamirz@gmail.com>
2023-12-13 18:59:11 +0330

    urls and views added for cafe app

---
+++ cafe/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('home', home, name='main'),
+    path('menu', menu, name='menu'),
+    path('cart', cart, name='cart'),
+    path('checkout', checkout, name='checkout'),
+    path('staff', staff, name='staff'),
+]
--- cafe/views.py
+++ cafe/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    if request.method == "GET":
+        return render(request, 'src/home.html', context={})
+
+
+def menu(request):
+    if request.method == "GET":
+        return render(request, 'src/menu.html', context={})
+
+
+def checkout(request):
+    if request.method == "GET":
+        return render(request, 'src/checkout.html', context={})
+
+
+def cart(request):
+    if request.method == "GET":
+        return render(request, 'src/cart.html', context={})
+
+
+def staff(request):
+    if request.method == "GET":
+        return render(request, 'src/staff.html', context={})
