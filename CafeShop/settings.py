ecd035d673f5bfb7eacab7fa378144e162123771
aliqnbri <aliqnbri1998@gmail.com>
2023-12-16 18:03:29 +0330

    authentication app added

--- CafeShop/settings.py
+++ CafeShop/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+<<<<<<< Updated upstream
+=======
+    'cafe',
+    'auth_app'
+>>>>>>> Stashed changes
 ]
 
 MIDDLEWARE = [
--- cafe/models.py
+++ cafe/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Account(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=11)
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField()
+
+
+class Tables(models.Model):
+    name = models.CharField(max_length=100)
+    capacity = models.IntegerField()
+    url = models.CharField()
+
+
+class Reservation(models.Model):
+    account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+    menu_id = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    table_id = models.ForeignKey(Tables, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_created=True)
+
+
+class Storage(models.Model):
+    material_title = models.CharField()
+    remined_material = models.IntegerField()
+    expire_date = models.DateField()
