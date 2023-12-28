321145699f915b9726e4c38c60a850792c142c57
ImanSMD <i.sadeghian1999@gmail.com>
2023-12-29 02:20:06 +0330

    data base deleted adn connected to new data base with new migrations

--- cafe/views.py
+++ cafe/views.py
@@ -15,16 +15,11 @@ from datetime import datetime
 def home(request):
     if request.method == "GET":
         return render(request, 'src/index.html', context={})
-    # if request.method == "POST":
-    #     search = request.POST.get('form1')
-    #     print("*********************")
-    #     print(search)
-    #     return render(request, 'src/index.html', context={})
+   
 
 
 def menu(request):
     if request.method == "GET":
-        # request.session.clear()
         all = Menu.objects.all()
         foods = Menu.objects.filter(category='lunch')
         foods_d = Menu.objects.filter(category='dinner')
@@ -33,16 +28,13 @@ def menu(request):
         return render(request, 'src/menu1.html', context={"foods": foods, 'foods_d': foods_d})
     if request.method == "POST":
         count = {}
-        # foods = Menu.objects.all()
         foods = Menu.objects.all()
         search = request.POST.get('search')
         for food in foods:
             counter = request.POST.get(food.title)
-            # price = food.price
             if counter is not None:
                 request.session[food.title] = counter
                 count[food.title] = counter
-                # request.session[f"{food.title}_price"] = price * int(counter)
             else:
                 if int(request.session[food.title]) > 0:
                     pass
@@ -50,12 +42,6 @@ def menu(request):
                     request.session[food.title] = 0
             if request.session[food.title] == 0:
                 del request.session[food.title]
-            print("*********************")
-            print(food.title)
-            # print(request.session[f"{food.title}_price"])
-            print(request.session[food.title])
-            print(count)
-        # print(data)
         if search is not None:
             foods = Menu.objects.filter(title__icontains=search)
         return render(request, 'src/menu1.html', context={"foods": foods, 'count': count})
@@ -67,12 +53,9 @@ def checkout(request):
         totall_price = 0
         foods = Menu.objects.all()
         for food in foods:
-            # if request.session[food.title] == '0':
-            #     del request.session[food.title]
+        
             if request.session[food.title] != '0':
-                totall_price += int(food.price) * \
-                    int(request.session[food.title])
-                # print(request.session[f"{food.title}_price"])
+                totall_price += int(food.price) * int(request.session[food.title])
                 names.append(food.title)
         reserv = Menu.objects.filter(title__in=names)
         return render(request, 'src/checkout.html', context={"reserv": reserv, 'price': totall_price})
@@ -80,29 +63,15 @@ def checkout(request):
 
 def cart(request):
     if request.method == "GET":
-        print(request.session.items())
         names = []
         totall_price = 0
         foods = Menu.objects.all()
         for food in foods:
-            # if request.session[food.title] == '0':
-            #     del request.session[food.title]
             if request.session[food.title] != '0':
                 totall_price += int(food.price) * \
                     int(request.session[food.title])
-                # print(request.session[f"{food.title}_price"])
                 names.append(food.title)
         reserv = Menu.objects.filter(title__in=names)
-        # account = Account(name='guest')
-        # account.save()
-        # table = Tables(pk=1)
-        # table.save()
-        # reserving = Reservation.objects.create(
-        #     account_id=account, table=table, reserved_time=datetime.now(), created_at=datetime.now())
-        # reserving.save()
-        # for i in reserv:
-        #     reserving.menu.add(i)
-        #     reserving.save()
         return render(request, 'src/cart.html', context={"foods": reserv, 'price': totall_price})
 
 
@@ -115,14 +84,6 @@ def service(request):
     if request.method == "GET":
         return render(request, 'src/service.html', context={})
 
-
-# @login_required
-# def staff(request):
-#     if request.method == "GET":
-#         reserves = Reservation.objects.all()
-#         storage = Storage.objects.filter(remined_material__lte=10)
-#         return render(request, 'src/staff.html', context={"reserves": reserves, "storage": storage})
-
 class StaffPanel(ListView,LoginRequiredMixin):
     model = Reservation
     template_name = 'src/staff.html'
@@ -131,20 +92,13 @@ class StaffPanel(ListView,LoginRequiredMixin):
         qs = qs.order_by("-id").reverse() 
         return qs
 
-
 def testimonial(request):
     if request.method == "GET":
         return render(request, 'src/testimonial.html', context={})
 
-
 def contact(request):
     if request.method == "GET":
         return render(request, 'src/contact.html', context={})
-
-# def booking(request):
-#     if request.method == "GET":
-#         return render(request, 'src/booking.html', context={})
-
 
 class booking(View):
     def get(self, request):
