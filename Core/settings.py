df827b8878e8a3d165bb2096a9bafb99f36721e1
ImanSMD <110537853+ImanSMD@users.noreply.github.com>
2024-01-17 00:23:15 +0330

    fixed : cart

--- Core/settings.py
+++ Core/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'multiforloop',
     'shop.apps.ShopConfig',
     'cafe.apps.CafeConfig',
     'cart.apps.CartConfig',
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,12 @@
 from typing import Any
-from django.urls import reverse
-from django.shortcuts import render, get_object_or_404
+from django.urls import reverse , reverse_lazy
+from django.shortcuts import render, get_object_or_404 , redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Category, Product
 from cart.forms import CartAddProductForm
-from django.views.generic import ListView ,DetailView
+from django.views import View
+from django.views.generic import ListView ,DetailView , TemplateView
+from cart.cart import Cart
 
 # Create your views here.
 
@@ -28,32 +30,36 @@ class ProductListView(ListView):
                 return Product.objects.filter(category = self.category,available=True , name__icontains=self.request.GET['search'] )
             return Product.objects.filter(available=True,category = self.category)
     def post(self, request, *args, **kwargs):
-        # print(self.request.POST)
-        override = (self.request.POST['override'])
-        quantity =self.request.POST['quantity']
-        form = CartAddProductForm(quantity,override)
-        # if form.is_valid():
-            # <process form cleaned data>
-        return reverse('cart:cart_add')    
+        print(self.request.POST)
+        cart = Cart(request)
+        product = get_object_or_404(Product,id =self.request.POST['product_id'])
+        form = CartAddProductForm(request.POST)
+        print(form)
+        if form.is_valid():   
+            cd = form.cleaned_data
+            cart.add(product=product,
+                    quantity=cd['quantity'],
+                    override_quantity=cd['override'])
+        return redirect('cart:cart_detail')    
             
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
         context["form"] =  CartAddProductForm()
-        return context            
+        return context          
 
 
 
     
-class ProductDetailView(DetailView):
-    queryset = Product.objects.filter(available = True)
-    template_name = 'shop/product/detail1.html'
-    slug_url_kwarg = "slug"
-    def get_context_data(self, **kwargs) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context["form"] =  CartAddProductForm()
-        print(context)
-        return context
+# class ProductDetailView(DetailView):
+#     queryset = Product.objects.filter(available = True)
+#     template_name = 'shop/product/detail1.html'
+#     slug_url_kwarg = "slug"
+#     def get_context_data(self, **kwargs) -> dict[str, Any]:
+#         context = super().get_context_data(**kwargs)
+#         context["form"] =  CartAddProductForm()
+#         print(context)
+#         return context
     
 
 
