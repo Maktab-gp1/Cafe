46eef4d6ae7f205ee8db52c341f6c6f3536bb480
Amirznd <zenderoohamirz@gmail.com>
2024-01-17 21:34:15 +0330

    filter by category added

--- staff/views.py
+++ staff/views.py
@@ -1,134 +1,207 @@
+from django.http import HttpResponse
+import datetime
+import csv
 from typing import Any
-from django.shortcuts import render 
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse_lazy 
+from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic.edit import UpdateView ,CreateView 
+from django.views.generic.edit import UpdateView, CreateView
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from orders.models import Order,OrderItem, Table
-from shop.models import Category , Product 
-from .mixin import OrderFieldsMixin,OrderItemFormValidMixin
-from .forms import  OrderItemStaffFormSet 
+from orders.models import Order, OrderItem, Table
+from shop.models import Category, Product
+from .mixin import OrderFieldsMixin, OrderItemFormValidMixin
+from .forms import OrderItemStaffFormSet
+from datetime import datetime
 
-  
-  
 
-    
-   
 @login_required
 def dashboard(request):
     context = {}
     context["products"] = Product.objects.all()
     context["categories"] = Category.objects.all()
     context['object_list'] = Order.objects.all().order_by('id')
-   
+
     if request.method == "POST":
         order_instance = Order.objects.filter(id=request.POST["id"]).first()
         order_instance.status = request.POST["status"]
-        order_instance.save() 
+        order_instance.save()
 
-    return render(request ,'staff/dashboard.html' , context=context )
+    return render(request, 'staff/dashboard.html', context=context)
+
+
 @login_required
 def tables(request):
     context = {}
     context['tables'] = Table.objects.all()
     if request.method == "POST":
         table_instance = Table.objects.filter(id=request.POST["id"]).first()
-        if "is_available" in request.POST :
+        if "is_available" in request.POST:
             table_instance.is_available = True
         else:
             table_instance.is_available = False
         table_instance.save()
-    return render(request,'staff/tablelist.html' , context=context)
+    return render(request, 'staff/tablelist.html', context=context)
 
-class TableCreateView(LoginRequiredMixin,CreateView):
+
+class TableCreateView(LoginRequiredMixin, CreateView):
     login_url = reverse_lazy('login')
-    model= Table
-    fields=['id' ,'name']
+    model = Table
+    fields = ['id', 'name']
     template_name = 'staff/tablecreate.html'
     success_url = reverse_lazy('tablelist')
 
-class OrderStaffUpdate(LoginRequiredMixin,UpdateView,OrderFieldsMixin,OrderItemFormValidMixin): 
+
+class OrderStaffUpdate(LoginRequiredMixin, UpdateView, OrderFieldsMixin, OrderItemFormValidMixin):
     login_url = reverse_lazy('login')
     model = Order
     fields = '__all__'
     template_name = 'staff/dashboard1.html'
     success_url = reverse_lazy('staff/dashboard')
+
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         if self.request.POST:
-            form =  OrderItemStaffFormSet(self.request.POST,instance=self.object)
+            form = OrderItemStaffFormSet(
+                self.request.POST, instance=self.object)
             data['order_item_staff_formset'] = form
             form.save()
         else:
-            data['order_item_staff_formset'] = OrderItemStaffFormSet(instance=self.object)
+            data['order_item_staff_formset'] = OrderItemStaffFormSet(
+                instance=self.object)
         return data
 
 
-class CategoryListView(LoginRequiredMixin,ListView):
+class CategoryListView(LoginRequiredMixin, ListView):
     login_url = reverse_lazy('login')
     model = Category
     template_name = 'staff/categorieslist.html'
 
-class CategoryUpdateView(LoginRequiredMixin,UpdateView):
+
+class CategoryUpdateView(LoginRequiredMixin, UpdateView):
     login_url = reverse_lazy('login')
     model = Category
-    fields= ['name']
+    fields = ['name']
     template_name = 'staff/categoriesview.html'
     success_url = reverse_lazy('dashboard')
-      
-class CategoryCreateView(LoginRequiredMixin,CreateView):
+
+
+class CategoryCreateView(LoginRequiredMixin, CreateView):
     login_url = reverse_lazy('login')
-    model= Category
-    fields=['name']
+    model = Category
+    fields = ['name']
     template_name = 'staff/categorycreate.html'
     success_url = reverse_lazy('dashboard')
 
-class ProductUpdateView(LoginRequiredMixin,UpdateView):
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     login_url = reverse_lazy('login')
     model = Product
-    fields= '__all__'
-    template_name = 'staff/newitem.html'
-    success_url = reverse_lazy('dashboard')
-      
-class ProductCreateView(LoginRequiredMixin,CreateView):
-    login_url = reverse_lazy('login')
-    model= Product
-    fields='__all__'
+    fields = '__all__'
     template_name = 'staff/newitem.html'
     success_url = reverse_lazy('dashboard')
 
 
-class Managerview(LoginRequiredMixin,View):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     login_url = reverse_lazy('login')
+    model = Product
+    fields = '__all__'
+    template_name = 'staff/newitem.html'
+    success_url = reverse_lazy('dashboard')
+
+
+class Managerview(View):
     template_name = 'staff/manager.html'
     model = OrderItem
 
-    def get(self, request):
-        count = dict()
-        summ = dict()
-        mountain_elevation_data = list()
+    def post(self, request):
+        if not request.user.is_superuser:
+            return redirect('dashboard')
+        if request.POST.get('category') == None:
+            start_time = datetime.strptime(
+                request.POST.get('trip-start'), "%Y-%m-%d")
+            end_time = datetime.strptime(
+                request.POST.get('trip-end'), "%Y-%m-%d")
+            category = None
+        else:
+            category = request.POST.get('category')
+        cat = Category.objects.all()
+        count, summ, time, hour = dict(), dict(), dict(), dict()
+        mountain_elevation_data, mountain_elevation_data_time = list(), list()
         for item in OrderItem.objects.all():
+            if request.POST.get('category') == None:
+                if start_time.date() <= item.order.created.date() <= end_time.date():
+                    if item.order.id not in time:
+                        time[item.order.id] = Order.objects.filter(
+                            id=item.order.id)
+                    count[item.product.name] = OrderItem.objects.filter(
+                        product_id=item.product.id)
+            else:
+                if item.order.id not in time:
+                    time[item.order.id] = Order.objects.filter(
+                        id=item.order.id)
+                count[item.product.name] = OrderItem.objects.filter(
+                    product_id=item.product.id)
+        for k, v in time.items():
+            for x in v:
+                if k in hour.keys():
+                    hour[k] += x.created.hour
+                else:
+                    hour[k] = x.created.hour
+        for k, v in count.items():
+            for x in v:
+                if category is not None:
+                    if x.product.category.name == category:
+                        if k in summ.keys():
+                            summ[k] += x.quantity
+                        else:
+                            summ[k] = x.quantity
+                else:
+                    if k in summ.keys():
+                        summ[k] += x.quantity
+                    else:
+                        summ[k] = x.quantity
+        for i in range(0, 25):
+            mountain_elevation_data_time.append({"label": i, "y": 0})
+            for k, v in hour.items():
+                if v == i:
+                    mountain_elevation_data_time[i]['y'] += 1
+        for k, v in summ.items():
+            mountain_elevation_data.append({"label": k, "y": v})
+        return render(request, self.template_name, context={"mountain_elevation_data": mountain_elevation_data, "mountain_elevation_data_time": mountain_elevation_data_time, 'category': cat})
+
+    def get(self, request):
+        if not request.user.is_superuser:
+            return redirect('dashboard')
+        count, summ, time, hour = dict(), dict(), dict(), dict()
+        mountain_elevation_data, mountain_elevation_data_time = list(), list()
+        cat = Category.objects.all()
+        for item in OrderItem.objects.all():
+            if item.order.id not in time:
+                time[item.order.id] = Order.objects.filter(id=item.order.id)
             count[item.product.name] = OrderItem.objects.filter(
                 product_id=item.product.id)
+        for k, v in time.items():
+            for x in v:
+                if k in hour.keys():
+                    hour[k] += x.created.hour
+                else:
+                    hour[k] = x.created.hour
         for k, v in count.items():
             for x in v:
                 if k in summ.keys():
                     summ[k] += x.quantity
                 else:
                     summ[k] = x.quantity
+        for i in range(0, 25):
+            mountain_elevation_data_time.append({"label": i, "y": 0})
+            for k, v in hour.items():
+                if v == i:
+                    mountain_elevation_data_time[i]['y'] += 1
         for k, v in summ.items():
             mountain_elevation_data.append({"label": k, "y": v})
-        return render(request, self.template_name, context={'count': summ, "mountain_elevation_data": mountain_elevation_data})
-    
-
-
-import csv
-import datetime
-from django.http import HttpResponse
-
-
+        return render(request, self.template_name, context={"mountain_elevation_data": mountain_elevation_data, "mountain_elevation_data_time": mountain_elevation_data_time, 'category': cat})
 
 
 def export_to_csv(modeladmin, request, queryset):
@@ -137,8 +210,8 @@ def export_to_csv(modeladmin, request, queryset):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = content_disposition
     writer = csv.writer(response)
-    fields = [field for field in opts.get_fields() if not \
-              field.many_to_many and not field.one_to_many] 
+    fields = [field for field in opts.get_fields() if not
+              field.many_to_many and not field.one_to_many]
     # Write a first row with header information
     writer.writerow([field.verbose_name for field in fields])
     # Write data rows
@@ -150,9 +223,11 @@ def export_to_csv(modeladmin, request, queryset):
     #             value = value.strftime('%d/%m/%Y')
     #         data_row.append(value)
     #     writer.writerow(data_row)
-    data_rows = [[getattr(obj, field.name).strftime('%d/%m/%Y') \
-        if isinstance(getattr(obj, field.name), datetime.datetime) \
-        else getattr(obj, field.name) for field in fields] for obj in queryset]
+    data_rows = [[getattr(obj, field.name).strftime('%d/%m/%Y')
+                  if isinstance(getattr(obj, field.name), datetime.datetime)
+                  else getattr(obj, field.name) for field in fields] for obj in queryset]
     writer.writerows(data_rows)
     return response
+
+
 export_to_csv.short_description = 'Export to CSV'
